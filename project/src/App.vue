8efa85db7f6cc7f862ce36d0148d327c63aa9259
Ximena Zaleta <ximenazaleta@hotmail.com>
2025-11-20 12:28:34 -0600

    feat: agregar componente Footer al proyecto

--- project/src/App.vue
+++ project/src/App.vue
@@ -1,9 +1,11 @@
 <script setup>
-  import BienvenidaView from "@/views/BienvenidaView.vue";
+import BienvenidaView from "@/views/BienvenidaView.vue";
+import Footer from "@/components/Footer.vue";
 </script>
 
 <template>
   <BienvenidaView></BienvenidaView>
+  <Footer />
 </template>
 
 <style scoped>
