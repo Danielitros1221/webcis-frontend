741c597a6feafc0abfa20b092c415c1dc879c364
dahna <dahnaelizabeth5389@gmail.com>
2026-01-07 13:19:18 -0600

    prueba

--- project/src/components/bienvenida/Presentacion.vue
+++ project/src/components/bienvenida/Presentacion.vue
@@ -3,6 +3,7 @@
 </script>
 
 <template>
+  <p>HOLA DESDE PRESENTACION</p>
 
 </template>
 
--- project/src/views/BienvenidaView.vue
+++ project/src/views/BienvenidaView.vue
@@ -1,10 +1,13 @@
 <script setup>
 
+import Presentacion from "@/components/bienvenida/Presentacion.vue";
 </script>
 
 <template>
   <div class="bg-amber-300 px-6 py-6">
     <p>Hola Mundo</p>
+    <p>Presentacion</p>
+    <presentacion></presentacion>
   </div>
 </template>
 
