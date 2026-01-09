09c8dc1e1315f549af5743ff4595482d6582cd17
dahna <dahnaelizabeth5389@gmail.com>
2026-01-08 20:37:07 -0600

    textos

--- project/src/components/bienvenida/Presentacion.vue
+++ project/src/components/bienvenida/Presentacion.vue
@@ -5,20 +5,25 @@
 <template>
   <div class="flex flex-col">
 
+    <!-- px-28.5 text-oro-ingenieril text-6xl font-bold -->
+
     <div class="bg-gradient-to-r from-black to-transparent border-2 w-130 h-125 py-25">
-      <h1 class="px-28.5 text-oro-ingenieril text-6xl font-bold">
-        WebCIS
+      <h1 class="ty-encabezado2 px-28.5 text-acento">
+          WebCIS
       </h1>
       <br>
       <div class="relative flex px-28">
-        <p class="text-gris-codigo">
+        <p class="text-texto-secundario">
           Diseñado para mejorar las bases de programación de los alumnos a través de contenidos
           educativos proporcionando un espacio de acceso a materiales que sirvan como complemento
           al aprendizaje en el aula.
         </p>
       </div>
       <br>
-      <button class="text-color text-fondo  bg-azul-depuracion   text-2xl "> ¡MAS INFORMACÍON! </button>
+
+    <div class="relative flex px-28">
+      <button class="light text-white bg-primario text-2xl"> ¡MAS INFORMACÍON! </button>
+    </div>
 
     </div>
 
