f07f5346de253624bf42c16c5b3fe12dbbbddc0c
Kiwi <diegosxn@gmail.com>
2026-01-09 14:27:06 -0600

    Se añaden las secciones la página principal, falta la funcionalidad como botones, placeholder para las imágenes

---
+++ project/src/components/bienvenida/Secciones.vue
@@ -0,0 +1,42 @@
+<script setup>
+
+</script>
+
+<template>
+  <div class=" contain margin:auto w-screen h-130">
+    <div class="center bg-primario w-105 h-105 ">
+      <h1 class="text-white text-6xl font-bold">
+        Categorías
+      </h1>
+    </div>
+    <div class="center bg-primario w-105 h-105">
+      <h1 class="text-white text-6xl font-bold">
+        WebCIS
+      </h1>
+    </div>
+    <div class="center bg-primario w-105 h-105">
+      <h1 class="text-white text-6xl font-bold">
+        Recursos
+      </h1>
+    </div>
+
+  </div>
+</template>
+
+<style scoped>
+.contain{
+  align-items: center;
+  justify-content: center;
+  display: flex;
+}
+.center {
+  background-image: url("@/assets/images/ph.png");
+  background-size: auto; /*placeholder*/
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin: 10px;
+  float: left;
+  border-radius: 15px;
+}
+</style>
--- project/src/views/BienvenidaView.vue
+++ project/src/views/BienvenidaView.vue
@@ -1,11 +1,15 @@
 <script setup>
 
 import Presentacion from "@/components/bienvenida/Presentacion.vue";
+import Secciones from "@/components/bienvenida/Secciones.vue";
 </script>
 
 <template>
   <div class="bg-amber-300 px-6 py-6">
     <presentacion></presentacion>
+  </div>
+  <div class="bg-primario">
+    <secciones></secciones>
   </div>
 </template>
 
