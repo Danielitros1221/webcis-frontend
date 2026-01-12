d0d48f19da9ceb665f0c29e7112227714d6a0cd6
Kiwi <diegosxn@gmali.com>
2026-01-12 14:24:36 -0600

    Se arreglan los tamaños de los botones, ahora son dinámicos y cambian dependiendo del tamaño de la pantalla

--- project/src/components/bienvenida/Secciones.vue
+++ project/src/components/bienvenida/Secciones.vue
@@ -3,18 +3,18 @@
 </script>
 
 <template>
-  <div class=" contain margin:auto w-screen h-130">
-    <div class="center bg-primario w-105 h-105 ">
+  <div class=" contain margin:auto w-screen h-auto">
+    <div class="center bg-primario">
       <h1 class="text-white text-6xl font-bold">
         Categorías
       </h1>
     </div>
-    <div class="center bg-primario w-105 h-105">
+    <div class="center bg-primario">
       <h1 class="text-white text-6xl font-bold">
         WebCIS
       </h1>
     </div>
-    <div class="center bg-primario w-105 h-105">
+    <div class="center bg-primario">
       <h1 class="text-white text-6xl font-bold">
         Recursos
       </h1>
@@ -30,13 +30,21 @@
   display: flex;
 }
 .center {
+  height: 100%;
+  width: 100%;
+  aspect-ratio: 1/1;
+  max-height: 90%;
+  max-width: 90%;
   background-image: url("@/assets/images/ph.png");
   background-size: auto; /*placeholder*/
   display: flex;
   justify-content: center;
   align-items: center;
-  margin: 10px;
+  margin: 15px;
   float: left;
   border-radius: 15px;
+  cursor: pointer;
+  transition-duration: 0.4s;
+
 }
 </style>
