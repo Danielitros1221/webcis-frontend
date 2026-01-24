345be51dc278fd1e5468bfaf9bcb51f5256b2427
luism <lynxcapu1221@gmail.com>
2026-01-24 09:36:18 -0600

    Añadiendo Sections

---
+++ project/src/components/home/Sections.vue
@@ -0,0 +1,100 @@
+<script setup>
+
+</script>
+
+<template>
+  <div class="bg-primario contain w-screen h-auto">
+
+    <div class="left bg-primario">
+      <h1 class="text-white text-6xl font-bold">
+        Categorías
+      </h1>
+    </div>
+    <div class="center bg-primario">
+      <h1 class="text-white text-6xl font-bold">
+        WebCIS
+      </h1>
+    </div>
+    <div class="right bg-primario">
+      <h1 class="text-white text-6xl font-bold">
+        Recursos
+      </h1>
+      <h1 class="text-white"></h1>
+    </div>
+
+  </div>
+</template>
+
+<style scoped>
+
+.contain{
+  margin: auto;
+  margin-top: 3%;
+  margin-bottom: 3%;
+  display: flex;
+  max-width: 100%;
+  border-radius: 20px;
+
+}
+
+.trigger:hover + .text {
+  opacity: 1;
+}
+
+.left {
+  height: 50%;
+  width: 50%;
+  aspect-ratio: 1/1;
+  background-image: url("@/assets/images/home/code.jpg");
+  background-size: cover;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin-left: 2.5%;
+  margin-right: 2.5%;
+  margin-top: 3%;
+  margin-bottom: 3%;
+  border-radius: 20px;
+  cursor: pointer;
+  transition-duration: 0.4s;
+}
+
+.center{
+  height: 50%;
+  width: 50%;
+  aspect-ratio: 1/1;
+  //background-image: url("@/assets/images/home/");
+  background-size: contain;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin-top: 3%;
+  margin-bottom: 3%;
+  border-radius: 20px;
+  cursor: pointer;
+  transition-duration: 0.4s;
+}
+
+.right {
+  height: 50%;
+  width: 50%;
+  aspect-ratio: 1/1;
+  background-image: url("@/assets/images/home/coding-stock-image.jpg");
+  background-size: cover;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin-left: 2.5%;
+  margin-right: 2.5%;
+  margin-top: 3%;
+  margin-bottom: 3%;
+  border-radius: 20px;
+  cursor: pointer;
+  transition-duration: 0.4s;
+}
+
+.center:hover{
+  transform: scale(1.05);
+}
+
+</style>
--- project/src/views/public/HomeView.vue
+++ project/src/views/public/HomeView.vue
@@ -1,11 +1,15 @@
 <script setup>
 
 import Presentation from "@/components/home/Presentation.vue";
+import Sections from "@/components/home/Sections.vue";
 </script>
 
 <template>
   <div>
     <presentation></presentation>
+  </div>
+  <div>
+    <sections></sections>
   </div>
 </template>
 
